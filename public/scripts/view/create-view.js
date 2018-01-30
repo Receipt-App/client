43fbc8da8f6e39075e4b6654012f73cf4a64ba65
warlordlizard <alexjbowers88@gmail.com>
2018-01-30 15:25:00 -0800

    adding stuff to views

--- public/scripts/view/create-view.js
+++ public/scripts/view/create-view.js
@@ -2,12 +2,27 @@
 
 (function (module) {
     var createView = {};
-    createView.init = () => {
+    createView.init = (ctx, next) => {
         $('#create-user').show();
         $('#upload').hide();
         $('#profile').hide();
         $('#login').hide();
         $('#about-us').hide();
+        next();
+    }
+
+    createView.new = (ctx, next) => {
+        $('#create-user').on('submit', function (e) {
+            e.preventDefault();
+            let data = {
+                name: e.target.name.value,
+                email: e.target.email.value,
+                username: e.target.username.value
+            };
+            ctx.newuser = data;
+            console.log('data from form', ctx.newuser);
+            next();
+        });
     }
     module.createView = createView;
 })(window);
--- public/scripts/view/login-view.js
+++ public/scripts/view/login-view.js
@@ -2,13 +2,24 @@
 
 (function(module) {
     var loginView = {};
-    loginView.init = () => {
+    loginView.init = (ctx, next) => {
         $('#create-user').hide();
         $('#upload').hide();
         $('#profile').hide();
         $('#login').show();
         $('#about-us').hide();
+        next();
     }
+    loginView.login = (ctx, next) => {
+        $('#login').on('submit', function (e) {
+        e.preventDefault();
+        ctx.currentUser = e.target.username.value;
+        console.log(e.target.username.value);
+        next();
+        });
+        console.log('login');
+    };
+
     module.loginView = loginView;
 })(window);
 
