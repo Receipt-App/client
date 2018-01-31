9833237c489d1b137c659a82e4fb920a38a8a2ad
ruskeezy <christian.ax.miller@gmail.com>
2018-01-31 14:57:23 -0800

    fixed client server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile('index.html', { root: './public' })
 })
 
