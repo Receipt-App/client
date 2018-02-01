cf59e9f6b28d7e944db30da81f47235c2a028bcc
andriiglukhyi <andrigluxii@gmail.com>
2018-01-31 18:22:09 -0800

    new

--- public/scripts/view/profile-view.js
+++ public/scripts/view/profile-view.js
@@ -3,14 +3,15 @@
 (function(module) {
   const profileView = {};
 
-  profileView.init = () => {
+  profileView.init = (ctx, next) => {
     $('#create-user').hide();
     $('#login').hide();
     $('#profile').show();
     $('#about-us').hide();
 
-
-    
+    ctx.userNow = localStorage.curentUser;
+    // console.log('profile get user from local storage' , ctx);
+    next();
   }
 
   module.profileView = profileView;
